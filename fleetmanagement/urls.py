54c6da58df2afb07984d382784952a4e35bacae9
Victoria Wasonga <victoria.wasonga@strathmore.edu>
2023-01-20 15:06:59 +0300

    Driver module done

--- fleetmanagement/urls.py
+++ fleetmanagement/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns=[
     path("",views.index,name="home"),
+    path("/drivers",views.drivers_view,name="drivers"),
+    path("/driver_add",views.driver_add,name="driver_add"),
+    path("/driver_edit",views.driver_edit,name="driver_edit"),
 ]
--- fleetmanagement/views.py
+++ fleetmanagement/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,"fleetmanagement/dashboard.html")
+
+#driver 
+def drivers_view(request):
+    return render(request,"fleetmanagement/drivers_view.html")
+
+def driver_add(request):
+    return render(request,"fleetmanagement/driver_add.html")
+
+def driver_edit(request):
+    return render(request,"fleetmanagement/driver_edit.html")
